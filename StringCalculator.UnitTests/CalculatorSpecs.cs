25a5e8e0c8fa30b22f22c5700040ced181932607
InvincibleBoris <sean.duffy@xtravirt.com>
2013-11-18 16:37:44 +0000

    Initial commit
    Initial commit of project - testing SpecsFor unit testing.

---
+++ StringCalculator.UnitTests/CalculatorSpecs.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Should;
+
+namespace StringCalculator.UnitTests.Core
+{
+    using Moq;
+
+    using SpecsFor;
+
+    public class CalculatorSpecs
+    {
+        
+        public class when_adding_an_empty_string : SpecsFor<Calculator>
+        {
+            private int _result;
+
+            protected override void Given()
+            {
+                GetMockFor<IUser>()
+                    .SetupGet(u => u.Name)
+                    .Returns("Sean");
+            }
+
+            protected override void When()
+            {
+                _result = SUT.Add(string.Empty);
+            }
+
+            [Test]
+            public void then_it_should_return_zero()
+            {
+                _result.ShouldEqual(0);
+            }
+
+            [Test]
+            public void then_it_should_write_zero_to_the_console()
+            {
+                GetMockFor<Calculator.IConsole>()
+                    .Verify(c => c.WriteLine(0));
+            }
+
+            [Test]
+            public void then_it_logs_a_transaction()
+            {
+                GetMockFor<Calculator.ITransactions>()
+                    .Verify(t => t.Record(string.Empty, 0));
+            }
+
+            [Test]
+            public void then_it_should_tell_the_user_hello()
+            {
+                GetMockFor<Calculator.IConsole>()
+                    .Verify(c => c.WriteLine("Hello, Sean"));
+            }
+        }
+
+        public class when_adding_more_than_one_string : SpecsFor<Calculator>
+        {
+            private int _result;
+
+            protected override void Given()
+            {
+                GetMockFor<IUser>()
+                    .Setup(u => u.Name)
+                    .Returns("Sean");
+            }
+
+            protected override void When()
+            {
+                const string testInput = "2,3,5";
+                _result = SUT.Add(testInput);
+            }
+
+            [Test]
+            public void then_it_should_return_a_valid_result()
+            {
+                _result.ShouldEqual(10);
+            }
+        }
+    }
+
+    /*
+    [TestFixture]
+    public class CalculatorSpecs
+    {
+        private Calculator SUT;
+
+        private Mock<Calculator.IConsole> MockConsole;
+
+        private Mock<Calculator.ITransactions> MockTransactions;
+
+        [SetUp]
+        public void CreateCalculator()
+        {
+            MockTransactions = new Mock<Calculator.ITransactions>();
+            MockConsole = new Mock<Calculator.IConsole>();
+            SUT = new Calculator(MockConsole.Object, MockTransactions.Object);
+        }
+
+        [Test]
+        public void Adding_an_empty_string_returns_zero()
+        {
+            var sum = SUT.Add(string.Empty);
+
+            sum.ShouldEqual(0);
+        }
+
+        [Test]
+        public void Adding_one_returns_one()
+        {
+            var sum = SUT.Add("1");
+
+            sum.ShouldEqual(1);
+        }
+
+        [Test]
+        public void Adding_two_returns_two()
+        {
+            var sum = SUT.Add("2");
+
+            sum.ShouldEqual(2);
+        }
+
+        [Test]
+        public void Adding_one_and_two_returns_three()
+        {
+            var sum = SUT.Add("1,2");
+
+            sum.ShouldEqual(3);
+        }
+
+        [Test]
+        public void Adding_one_two_three_returns_six()
+        {
+            var sum = SUT.Add("1,2,3");
+
+            sum.ShouldEqual(6);
+        }
+
+        [Test]
+        public void Adding_negative_number_throws_exception()
+        {
+            Assert.Throws<InvalidOperationException>(() => SUT.Add("-1"));
+        }
+
+        [Test]
+        public void Adding_zero_writes_zero_to_the_console()
+        {
+            var sum = SUT.Add("0");
+
+            MockConsole.Verify(c => c.WriteLine(0));
+        }
+
+        [Test]
+        public void Adding_numbers_records_the_input_and_output()
+        {
+            var sum = SUT.Add("1,3");
+
+            MockTransactions.Verify(t => t.Record("1,3", 4));
+        }
+
+        [Test]
+        public void Adding_numbers_writes_message_to_the_console()
+        {
+            SUT.Add("1,2,3");
+
+            MockConsole.Verify(c => c.WriteLine("You owe us money!"));
+        }
+    }
+     */
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,71 @@
+﻿namespace StringCalculator
+{
+    using System;
+    using System.Linq;
+
+    public class Calculator
+    {
+        private readonly IConsole _console;
+
+        private readonly ITransactions _transactions;
+
+        private readonly IUser _user;
+
+        public Calculator(IConsole console, ITransactions transactions, IUser user)
+        {
+            _console = console;
+            _transactions = transactions;
+            _user = user;
+        }
+
+        public int Add(string input)
+        {
+            if (input.Contains("-"))
+            {
+                throw new InvalidOperationException();
+            }
+
+            var parts = input.Split(new [] {','}, StringSplitOptions.RemoveEmptyEntries);
+
+            var sum = parts.Sum(p => int.Parse(p));
+
+            _console.WriteLine(sum);
+            _transactions.Record(input, sum);
+            _console.WriteLine("You owe us money!");
+            _console.WriteLine("Hello, " + _user.Name );
+
+            return sum;
+
+        }
+
+        public interface ITransactions
+        {
+            void Record(string input, int sum);
+        }
+
+        public interface IConsole
+        {
+            void WriteLine(object o);
+        }
+
+        private static void Main(string[] args)
+        {
+            //var calc = new Calculator();
+            //var result = calc.Add(string.Empty);
+            //Console.WriteLine(result);
+
+            //var myStrings = new[] {"3", "5", "1"};
+            //var result = calc.Add(myStrings);
+            //Console.WriteLine(result);
+        }
+    }
+
+    public interface IUser
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+    }
+}
